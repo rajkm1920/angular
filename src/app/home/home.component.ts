a0250505fcf017dff5c13c7384ecf337bd977678
Raj Kumar <raj.maurya@picsaxis.com>
2017-11-22 12:09:47 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations : [
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35%)', offset: .3 }),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
+          ]))
+        ]), { optional: true}),
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35%)', offset: .3 }),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1 }),
+          ]))
+        ]), { optional: true})
+
+
+
+      ])
+
+
+
+
+
+    ])
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number;
+  btnTxt: string;
+  goalText: string;
+  temp: number;
+  goals= ['My first life goal', 'I want become a SE', 'Thank You'];
+  constructor() { }
+  ngOnInit() {
+    this.goalText  = 'My First life goal';
+    this.btnTxt = 'Add an Item';
+    this.itemCount = this.goals.length;
+  }
+addItem() {
+    if ( this.goalText.trim().length > 0) {
+      this.goals.push(this.goalText);
+      this.goalText = '';
+      this.itemCount =   this.goals.length;
+    }
+  }
+  removeItem(i) {
+    this.temp = i;
+    this.goals.splice(i, 1);
+  }
+}
